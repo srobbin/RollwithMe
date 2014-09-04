492cdd11b7a02aa1d89676ac82e8de380486c661
Scott Robbin <scott@robbin.co>
2014-09-04 18:02:12 -0500

    Step directions helper.

---
+++ app/helpers/directions_helper.rb
@@ -0,0 +1,10 @@
+module DirectionsHelper
+  def step_directions(step)
+    content_tag :div, class: 'well' do
+      step['detail'].each do |detail|
+        instruction = detail['html_instructions'].html_safe
+        concat content_tag(:p, instruction)
+      end
+    end
+  end
+end
