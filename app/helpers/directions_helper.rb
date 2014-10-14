59e6142dbf659147760da08e7978dda714f065e0
Scott Robbin <scott@robbin.co>
2014-10-13 21:02:03 -0500

    Fixed directions_helper that was causing duplicates.

--- app/helpers/directions_helper.rb
+++ app/helpers/directions_helper.rb
@@ -2,11 +2,12 @@ module DirectionsHelper
   def step_directions(step)
     content_tag :div, class: 'details' do
       step['detail'].each_with_index do |detail, index|
-        instruction = detail['html_instructions'].html_safe
-        concat content_tag(:p, instruction) + tag(:hr, class: 'subtle-hr')
         if index == step['detail'].size - 1
           instruction = detail['html_instructions'].html_safe
           concat content_tag(:p, instruction)
+        else
+          instruction = detail['html_instructions'].html_safe
+          concat content_tag(:p, instruction) + tag(:hr, class: 'subtle-hr')
         end
       end
     end
